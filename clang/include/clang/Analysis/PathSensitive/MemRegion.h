NanQin555/llvm-project

commit 153d3015e95b78cf47dba2c953a421dcde98de37

silence warning in release-asserts mode about unused variable, also move comment into assert message.
llvm-svn: 60938

clang/include/clang/Analysis/PathSensitive/MemRegion.h
@@ -420,9 +420,9 @@ class ElementRegion : public TypedRegion {
 
   ElementRegion(SVal Idx, const MemRegion* sReg)
     : TypedRegion(sReg, ElementRegionKind), Index(Idx) {
-    // The index must be signed.
-    if (nonloc::ConcreteInt* CI = dyn_cast<nonloc::ConcreteInt>(&Idx))
-      assert(CI->getValue().isSigned());
+    assert(isa<nonloc::ConcreteInt>(&Idx) &&
+           cast<nonloc::ConcreteInt>(&Idx)->getValue().isSigned() &&
+           "The index must be signed");
   }
 
   static void ProfileRegion(llvm::FoldingSetNodeID& ID, SVal Idx, 

commit 040860f2073d2aca225649bc0eec650d42c1a683

fix breakage I introduced in r60938
llvm-svn: 60941

clang/include/clang/Analysis/PathSensitive/MemRegion.h
@@ -420,8 +420,8 @@ class ElementRegion : public TypedRegion {
 
   ElementRegion(SVal Idx, const MemRegion* sReg)
     : TypedRegion(sReg, ElementRegionKind), Index(Idx) {
-    assert(isa<nonloc::ConcreteInt>(&Idx) &&
-           cast<nonloc::ConcreteInt>(&Idx)->getValue().isSigned() &&
+    assert((!isa<nonloc::ConcreteInt>(&Idx) ||
+           cast<nonloc::ConcreteInt>(&Idx)->getValue().isSigned()) &&
            "The index must be signed");
   }
 