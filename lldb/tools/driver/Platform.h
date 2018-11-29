NanQin555/llvm-project

commit 24a738709a98e946c3501bffbaee41b50d2e72de

Remove getopt includes from the driver
They are not needed now that we use LLVMOption for command-line parsing
thank you, Jonas).  This also allows us to avoid linking of lldbHost
into the driver which was breaking liblldb encapsulation.

(Technically, there is still a lldb/Host/windows/windows.h include which
is needed on windows, but this is a header-only wrapper for <windows.h>,
so it is not necessary to link lldbHost for that. But ideally, that
should go away too.)

llvm-svn: 347846

lldb/tools/driver/Platform.h
@@ -12,7 +12,6 @@
 
 #if defined(_WIN32)
 
-#include "lldb/Host/HostGetOpt.h"
 #include <io.h>
 #if defined(_MSC_VER)
 #include <signal.h>
@@ -74,7 +73,6 @@ extern int tcsetattr(int fd, int optional_actions,
 extern int tcgetattr(int fildes, struct termios *termios_p);
 
 #else
-#include "lldb/Host/HostGetOpt.h"
 #include <inttypes.h>
 
 #include <libgen.h>

lldb/tools/driver/CMakeLists.txt
@@ -2,23 +2,12 @@ set(LLVM_TARGET_DEFINITIONS Options.td)
 tablegen(LLVM Options.inc -gen-opt-parser-defs)
 add_public_tablegen_target(LLDBOptionsTableGen)
 
-if ((CMAKE_SYSTEM_NAME MATCHES "Windows") OR
-    (CMAKE_SYSTEM_NAME MATCHES "NetBSD" ))
-  # These targets do not have getopt support, so they rely on the one provided by
-  # liblldb. However, getopt is not a part of the liblldb interface, so we have
-  # to link against the constituent libraries manually. Note that this is
-  # extremely scary as it introduces ODR violations, and it should go away as
-  # soon as possible.
-  set(host_lib lldbHost)
-endif()
-
 add_lldb_tool(lldb
   Driver.cpp
   Platform.cpp
 
   LINK_LIBS
     liblldb
-    ${host_lib}
 
   LINK_COMPONENTS
     Option

commit 0e650af84d98a00aa90e1efbbc65ba01ee9cbec9

Fix windows build broken by r347846
The changed order of includes caused compile errors on MSVC due to
snprintf macro definition. snprintf should available since VS2015, and
the rest of the code seems to be able to use snprintf just fine without
this macro, so this removes it from the lldb driver as well.

llvm-svn: 347855

lldb/tools/driver/Platform.h
@@ -60,7 +60,6 @@ struct timeval {
   long tv_usec;
 };
 typedef long pid_t;
-#define snprintf _snprintf
 #define PATH_MAX MAX_PATH
 #endif
 