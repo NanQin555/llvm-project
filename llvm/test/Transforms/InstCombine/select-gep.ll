carbon-language/carbon-lang

commit d561b6fbdbe6d1da05fd92003a4ac1e37bf4b8bc

[InstCombine] Fold (select C, (gep Ptr, Idx), Ptr) -> (gep Ptr, (select C, Idx, 0)) (PR50183) (REAPPLIED)
As discussed on PR50183, we already fold to prefer 'select-of-idx' vs 'select-of-gep':

define <4 x i32>* @select0a(<4 x i32>* %a0, i64 %a1, i1 %a2, i64 %a3) {
  %gep0 = getelementptr inbounds <4 x i32>, <4 x i32>* %a0, i64 %a1
  %gep1 = getelementptr inbounds <4 x i32>, <4 x i32>* %a0, i64 %a3
  %sel = select i1 %a2, <4 x i32>* %gep0, <4 x i32>* %gep1
  ret <4 x i32>* %sel
}
-->
define <4 x i32>* @select1a(<4 x i32>* %a0, i64 %a1, i1 %a2, i64 %a3) {
  %sel = select i1 %a2, i64 %a1, i64 %a3
  %gep = getelementptr inbounds <4 x i32>, <4 x i32>* %a0, i64 %sel
  ret <4 x i32>* %gep
}

This patch adds basic handling for the 'fallthrough' cases where the gep idx == 0 has been folded away to the base address:

define <4 x i32>* @select0(<4 x i32>* %a0, i64 %a1, i1 %a2) {
  %gep = getelementptr inbounds <4 x i32>, <4 x i32>* %a0, i64 %a1
  %sel = select i1 %a2, <4 x i32>* %a0, <4 x i32>* %gep
  ret <4 x i32>* %sel
}
-->
define <4 x i32>* @select1(<4 x i32>* %a0, i64 %a1, i1 %a2) {
  %sel = select i1 %a2, i64 0, i64 %a1
  %gep = getelementptr inbounds <4 x i32>, <4 x i32>* %a0, i64 %sel
  ret <4 x i32>* %gep
}

Reapplied with a fix for the bpf "-bpf-disable-avoid-speculation" tests

Differential Revision: https://reviews.llvm.org/D105901

llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
@@ -2933,6 +2933,33 @@ Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
   if (Instruction *I = foldSelectExtConst(SI))
     return I;
 
+  // Fold (select C, (gep Ptr, Idx), Ptr) -> (gep Ptr, (select C, Idx, 0))
+  // Fold (select C, Ptr, (gep Ptr, Idx)) -> (gep Ptr, (select C, 0, Idx))
+  auto SelectGepWithBase = [&](GetElementPtrInst *Gep, Value *Base,
+                               bool Swap) -> GetElementPtrInst * {
+    Value *Ptr = Gep->getPointerOperand();
+    if (Gep->getNumOperands() != 2 || Gep->getPointerOperand() != Base ||
+        !Gep->hasOneUse())
+      return nullptr;
+    Type *ElementType = Gep->getResultElementType();
+    Value *Idx = Gep->getOperand(1);
+    Value *NewT = Idx;
+    Value *NewF = Constant::getNullValue(Idx->getType());
+    if (Swap)
+      std::swap(NewT, NewF);
+    Value *NewSI =
+        Builder.CreateSelect(CondVal, NewT, NewF, SI.getName() + ".idx", &SI);
+    return Gep->isInBounds()
+               ? GetElementPtrInst::CreateInBounds(ElementType, Ptr, {NewSI})
+               : GetElementPtrInst::Create(ElementType, Ptr, {NewSI});
+  };
+  if (auto *TrueGep = dyn_cast<GetElementPtrInst>(TrueVal))
+    if (auto *NewGep = SelectGepWithBase(TrueGep, FalseVal, false))
+      return NewGep;
+  if (auto *FalseGep = dyn_cast<GetElementPtrInst>(FalseVal))
+    if (auto *NewGep = SelectGepWithBase(FalseGep, TrueVal, true))
+      return NewGep;
+
   // See if we can fold the select into one of our operands.
   if (SelType->isIntOrIntVectorTy() || SelType->isFPOrFPVectorTy()) {
     if (Instruction *FoldI = foldSelectIntoOp(SI, TrueVal, FalseVal))

llvm/test/Transforms/InstCombine/select-gep.ll
@@ -74,9 +74,9 @@ define i32* @test2(i32* %p, i64 %x, i64 %y) {
 ; PR50183
 define i32* @test2a(i32* %p, i64 %x, i64 %y) {
 ; CHECK-LABEL: @test2a(
-; CHECK-NEXT:    [[GEP:%.*]] = getelementptr inbounds i32, i32* [[P:%.*]], i64 [[X:%.*]]
-; CHECK-NEXT:    [[CMP:%.*]] = icmp ugt i64 [[X]], [[Y:%.*]]
-; CHECK-NEXT:    [[SELECT:%.*]] = select i1 [[CMP]], i32* [[GEP]], i32* [[P]]
+; CHECK-NEXT:    [[CMP:%.*]] = icmp ugt i64 [[X:%.*]], [[Y:%.*]]
+; CHECK-NEXT:    [[SELECT_IDX:%.*]] = select i1 [[CMP]], i64 [[X]], i64 0
+; CHECK-NEXT:    [[SELECT:%.*]] = getelementptr inbounds i32, i32* [[P:%.*]], i64 [[SELECT_IDX]]
 ; CHECK-NEXT:    ret i32* [[SELECT]]
 ;
   %gep = getelementptr inbounds i32, i32* %p, i64 %x
@@ -88,9 +88,9 @@ define i32* @test2a(i32* %p, i64 %x, i64 %y) {
 ; PR50183
 define i32* @test2b(i32* %p, i64 %x, i64 %y) {
 ; CHECK-LABEL: @test2b(
-; CHECK-NEXT:    [[GEP:%.*]] = getelementptr inbounds i32, i32* [[P:%.*]], i64 [[X:%.*]]
-; CHECK-NEXT:    [[CMP:%.*]] = icmp ugt i64 [[X]], [[Y:%.*]]
-; CHECK-NEXT:    [[SELECT:%.*]] = select i1 [[CMP]], i32* [[P]], i32* [[GEP]]
+; CHECK-NEXT:    [[CMP:%.*]] = icmp ugt i64 [[X:%.*]], [[Y:%.*]]
+; CHECK-NEXT:    [[SELECT_IDX:%.*]] = select i1 [[CMP]], i64 0, i64 [[X]]
+; CHECK-NEXT:    [[SELECT:%.*]] = getelementptr inbounds i32, i32* [[P:%.*]], i64 [[SELECT_IDX]]
 ; CHECK-NEXT:    ret i32* [[SELECT]]
 ;
   %gep = getelementptr inbounds i32, i32* %p, i64 %x

llvm/test/CodeGen/BPF/adjust-opt-speculative1.ll
@@ -49,12 +49,12 @@ if.end:                                           ; preds = %if.then, %entry
 ; CHECK:         [[LABEL]]:
 ; CHECK:         r0 = [[REG6]]
 
-; CHECK-DISABLE: r0 = [[REG6]]
-; CHECK-DISABLE: r0 += [[REG1:r[0-9]+]]
-; CHECK-DISABLE: [[REG2:r[0-9]+]] = 8
-; CHECK-DISABLE: if [[REG2]] > [[REG1]] goto [[LABEL:.*]]
-; CHECK-DISABLE: r0 = [[REG6]]
+; CHECK-DISABLE: [[REG1:r[0-9]+]] = 8
+; CHECK-DISABLE: if [[REG1]] > r0 goto [[LABEL:.*]]
+; CHECK-DISABLE: r0 = 0
 ; CHECK-DISABLE: [[LABEL]]:
+; CHECK-DISABLE: [[REG6]] += r0
+; CHECK-DISABLE: r0 = [[REG6]]
 
 ; CHECK-COMMON:  exit
 

llvm/test/CodeGen/BPF/adjust-opt-speculative2.ll
@@ -56,12 +56,14 @@ if.end:                                           ; preds = %if.then, %entry
 ; CHECK-DISABLE: [[REG1:r[0-9]+]] = r0
 ; CHECK-DISABLE: [[REG1]] <<= 32
 ; CHECK-DISABLE: [[REG1]] >>= 32
-; CHECK-DISABLE: r0 = [[REG6]]
-; CHECK-DISABLE: r0 += [[REG1]]
 ; CHECK-DISABLE: [[REG2:r[0-9]+]] = 8
 ; CHECK-DISABLE: if [[REG2]] > [[REG1]] goto [[LABEL:.*]]
-; CHECK-DISABLE: r0 = [[REG6]]
+; CHECK-DISABLE: r0 = 0
 ; CHECK-DISABLE: [[LABEL]]:
+; CHECK-DISABLE: r0 <<= 32
+; CHECK-DISABLE: r0 >>= 32
+; CHECK-DISABLE: [[REG6]] += r0
+; CHECK-DISABLE: r0 = [[REG6]]
 
 ; CHECK-COMMON:  exit
 

NanQin555/llvm-project

commit a2190abe76a83c1b2c9149774d93baa0be65be16

Don't fold (select C, (gep Ptr, Idx), Ptr) if C is vector but Idx is scalar
The folding rule (select C, (gep Ptr, Idx), Ptr) -> (gep Ptr, (select C,
Idx, 0)) creates a malformed SELECT IR if C is a vector while Idx is scalar.

  SELECT VecC, ScalarIdx, 0

We could splat Idx to a vector but it defeats the purpose of
optimisation. Don't apply the folding rule in this case.

This fixes a regression from commit d561b6fbdbe6d1da05fd92003a4ac1e37bf4b8bc.

llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
@@ -3004,8 +3004,10 @@ Instruction *InstCombinerImpl::visitSelectInst(SelectInst &SI) {
     if (Gep->getNumOperands() != 2 || Gep->getPointerOperand() != Base ||
         !Gep->hasOneUse())
       return nullptr;
-    Type *ElementType = Gep->getResultElementType();
     Value *Idx = Gep->getOperand(1);
+    if (isa<VectorType>(CondVal->getType()) && !isa<VectorType>(Idx->getType()))
+      return nullptr;
+    Type *ElementType = Gep->getResultElementType();
     Value *NewT = Idx;
     Value *NewF = Constant::getNullValue(Idx->getType());
     if (Swap)

llvm/test/Transforms/InstCombine/select-gep.ll
@@ -244,3 +244,16 @@ define i32* @test6(i32* %p, i64 %x, i64 %y) {
   ret i32* %sel
 }
 declare void @use_i32p(i32*)
+
+; We cannot create a select-with-idx with a vector condition but scalar idx.
+
+define <2 x i64*> @test7(<2 x i64*> %p1, i64 %idx, <2 x i1> %cc) {
+; CHECK-LABEL: @test7(
+; CHECK-NEXT:    [[GEP:%.*]] = getelementptr i64, <2 x i64*> [[P1:%.*]], i64 [[IDX]]
+; CHECK-NEXT:    [[SELECT:%.*]] = select <2 x i1> [[CC:%.*]], <2 x i64*> [[P1:%.*]], <2 x i64*> [[GEP]]
+; CHECK-NEXT:    ret <2 x i64*> [[SELECT]]
+;
+  %gep = getelementptr i64, <2 x i64*> %p1, i64 %idx
+  %select = select <2 x i1> %cc, <2 x i64*> %p1, <2 x i64*> %gep
+  ret <2 x i64*> %select
+}